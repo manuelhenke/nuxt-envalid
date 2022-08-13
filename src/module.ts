6782ab6fac9cc8072e3fd37eca6e7060c388354c
Manuel Henke <manuel.henke@yahoo.de>
2022-08-13 21:55:18 +0200

    ci: run ci for different node version

--- src/module.ts
+++ src/module.ts
@@ -4,12 +4,14 @@ import { name, version } from '../package.json'
 import { Options } from './types'
 
 export * from 'envalid/dist/validators'
+export * from 'envalid/dist/errors'
+export { testOnly } from 'envalid'
 
 export type ModuleOptions = Options | (() => Options)
 
 const CONFIG_KEY = 'envalid'
 
-async function getOptions (moduleOptions: ModuleOptions): Promise<ModuleOptions> {
+async function getOptions (moduleOptions: ModuleOptions): Promise<Options> {
   if (typeof moduleOptions === 'function') {
     moduleOptions = await moduleOptions.call(this)
   }
@@ -29,16 +31,19 @@ async function getOptions (moduleOptions: ModuleOptions): Promise<ModuleOptions>
 }
 
 const nuxtModule: Module<ModuleOptions> = async function (moduleOptions) {
-  const options: Options = await getOptions.call(this, moduleOptions)
+  const {
+    options,
+    specs
+  }: Options = await getOptions.call(this, moduleOptions)
 
   const env = {
     ...process.env,
     ...this.options.env
   }
 
-  const cleanEnvConfig = cleanEnv(env, options.specs, options.options)
+  const cleanEnvConfig = cleanEnv(env, specs, options)
 
-  Object.keys(options.specs).forEach((key) => {
+  Object.keys(specs).forEach((key) => {
     this.options.env[key] = cleanEnvConfig[key]
     process.env[key] = cleanEnvConfig[key]
   })
